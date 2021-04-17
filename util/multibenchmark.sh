35fe49834103240226007f387ec17d718c90535e
Chris White <cxwembedded@gmail.com>
2021-04-16 20:57:14 -0400

    multibenchmark: go back to non-parallel
    I see very wide variance on my system when running tests in parallel,
    so stick with serial for now.

--- util/multibenchmark.sh
+++ util/multibenchmark.sh
@@ -20,22 +20,11 @@ main() {
   echo "# $tag ($stem) ($logfn)" > "$logfn"
 
   # Run the benchmarks
-  local -r nprocs="$(nproc)"
-  local -r nsets="$((nruns/nprocs))"
-  if (( nsets < 1 )); then
-    nsets=1
-  fi
-  echo "$nruns run(s) on $nprocs processor(s) => $nsets set(s)" > "$logfn"
-
   set -x
-  for setidx in `seq 0 $((nsets - 1))` ; do
-    export first="$((setidx*nprocs))"
-    set +eo pipefail  # because taskset may fail to set affinity
-    parallel --ungroup taskset -c {} \
-      ./util/benchmark.py --generate-baseline -l wren \
-        --baseline-filename "$stem-{= $first + \$_ =}.txt" \
-      ':::' `seq 0 "$nprocs"`
-    set -eo pipefail
+  local i
+  for i in `seq 0 $((nruns - 1))` ; do
+    ./util/benchmark.py --generate-baseline -l wren \
+        --baseline-filename "$stem-$i.txt"
   done 2>&1 |tee -a "$logfn"
 
   # Aggregate the data
@@ -45,7 +34,11 @@ main() {
   for i in `seq 0 $((nruns - 1))` ; do  # can't do this in the loop above
     fns+=("$stem-$i.txt")               # because the loop body is a subshell
   done
-  perl -M5.024 -Mvars='$results' -Mstrict -Mwarnings -F, -E 'chomp @F; push $results->{$F[0]}->@*, $F[1]; END { say join(q(,), qq("\Q$_\E"), $results->{$_}->@*) foreach sort keys %$results}' "${fns[@]}" > "$stem-summary.csv"
+
+  perl -M5.024 -Mvars='$results' -Mstrict -Mwarnings -F, -E '
+    chomp @F; push $results->{$F[0]}->@*, $F[1];
+    END { say join(q(,), qq("\Q$_\E"), $results->{$_}->@*) foreach sort keys %$results}' \
+      "${fns[@]}" > "$stem-summary.csv"
 
   set +x
 } #main()
