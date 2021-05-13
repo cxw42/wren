745723e1aa8510c242b1266d7526dbf22e23259d
Chris White <cxwembedded@gmail.com>
2021-05-12 20:58:11 -0400

    speedtrial: add --csv, --cpu-secs

--- util/speedtrial.pl
+++ util/speedtrial.pl
@@ -6,7 +6,7 @@ speedtrial.pl - Compare speed of multiple Wren files
 
 =head1 SYNOPSIS
 
-  speedtrial.pl STEM
+  speedtrial.pl [OPTIONS] STEM
 
 Runs all C<< <STEM>*.wren >> tests and reports the results.
 
@@ -15,10 +15,12 @@ Runs all C<< <STEM>*.wren >> tests and reports the results.
 use 5.006;
 use strict;
 use warnings;
+use autodie;
 use Benchmark qw(cmpthese timethese);
 use Config;
 use File::Spec;
 use FindBin;
+use Getopt::Long qw(GetOptionsFromArray :config gnu_getopt);
 use Pod::Usage qw(pod2usage);
 
 exit main(@ARGV);
@@ -52,10 +54,9 @@ BEGIN {
 ### main ####################################################################
 
 sub main {
-  pod2usage(2) unless @_ == 1;
-  my $stem = shift;
-  my @tests = <$stem*.wren>;
-  die "Couldn't find any tests for $stem" unless @tests;
+  my %opts = parse_args(@_);
+  my @tests = <$opts{stem}*.wren>;
+  die "Couldn't find any tests for $opts{stem}" unless @tests;
 
   # Find wren_test
   my $wren_test = find_wren_test();
@@ -65,7 +66,7 @@ sub main {
   my $starttime = time();
   $| = 1;
 
-  my $results = timethese(-4,  # -4 => at least 4 sec. CPU time per test
+  my $results = timethese(-$opts{cpu_secs},
     +{
       map { (File::Spec->splitpath($_))[2] => "print '.'; die 'test failed' if system q($wren_test), q($_)" } @tests
     }
@@ -75,15 +76,48 @@ sub main {
   print("Ran for $duration seconds using $wren_test\n");
   cmpthese($results); # detailed report
 
-  # CSV summary.  Thanks to Benchmark::cmpthese() for the math below.
-  my $report = qq("$stem");
-  for my $test (sort keys %$results) {
-    $report .= qq(,"$test",) . $results->{$test}->iters/($results->{$test}->elapsed+0.000000000000001);
-  }
-  print "$report\n";  # should be the only thing matching /^"/ in the log file.
+  report_csv($opts{csv}, $opts{stem}, $results) if $opts{csv};
 
   return 0;
 } #main()
+
+sub parse_args {
+  my %opts;
+
+  # Easier syntax for checking whether optional args were provided.
+  # Syntax thanks to http://www.perlmonks.org/?node_id=696592
+  local *have = sub { return exists($opts{ $_[0] }); };
+
+  GetOptionsFromArray(\@_, \%opts,
+      'usage|?', 'h|help', 'man',    # options we handle here
+      'csv=s', 'cpu_secs|cpu-secs=s',
+  ) or pod2usage(2);
+  pod2usage(2) unless @_ == 1;
+  $opts{stem} = shift;
+
+  pod2usage(-verbose => 0, -exitval => 0) if have('usage');
+  pod2usage(-verbose => 1, -exitval => 0) if have('h');
+  pod2usage(-verbose => 2, -exitval => 0) if have('man');
+
+  $opts{cpu_secs} ||= 4;  # default CPU time, in seconds, per test
+
+  return %opts;
+}
+
+### Helpers #################################################################
+
+sub report_csv {
+  my ($fn, $stem, $results) = @_;
+
+  # CSV summary.  Thanks to Benchmark::cmpthese() for the math below.
+  my $report = qq("$stem","@{[scalar localtime]}");
+  for my $test (sort keys %$results) {
+    $report .= qq(,"$test",) . $results->{$test}->iters/($results->{$test}->elapsed+0.000000000000001);
+  }
+  open my $fh, '>>', $fn;
+  print $fh "$report\n";  # should be the only thing matching /^"/ in the log file.
+  close $fh;
+}
 
 sub find_wren_test {
   my ($vol, $dirs, $file) = File::Spec->splitpath($FindBin::Bin);
@@ -99,6 +133,17 @@ sub find_wren_test {
 } #find_wren_test()
 
 ### Docs ####################################################################
+__END__
+
+=head1 OPTIONS
+
+=head2 B<--cpu-secs> C<secs>
+
+Run for C<secs> seconds of CPU time instead of the default.
+
+=head2 B<--csv> C<file>
+
+Append a summary of the results to C<file>.
 
 =head1 AUTHOR
 
